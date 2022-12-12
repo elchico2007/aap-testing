7a57044ddcdf18dbeb3447dcb590fba25434186a
Luis Valle <umblander06@gmail.com>
2022-12-11 20:59:46 -0700

    Adding terraform test

---
+++ project/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  tags = {
+    Name = var.name
+  }
+}
